6cbf6fa6b8c1943b122e976eccdb2e651c5d1166
Razu <jahangir8988@gmail.com>
2019-03-18 23:59:00 +0600

    OK

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,8 +3,7 @@ import { link } from 'fs';
 
 class  Counter extends Component {
     state = {
-        count: 0,
-        tags:['tag1','tag2','tag3']
+        value: this.props.value
     };
     styles = {
         fontSize:50,
@@ -17,14 +16,16 @@ class  Counter extends Component {
     // }
     handleIncrement =(product) =>{
         console.log(product);
-       this.setState({count:this.state.count+1});
+       this.setState({value:this.state.value+1});
     }
     doHandleIncrement = () =>{
         this.setState({id:1});
     }
     render() { 
+        console.log(this.props);
         return (
         <div>
+            {this.props.children}
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick={ () => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>
             {/* <ul>
@@ -36,12 +37,12 @@ class  Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const{count}=this.state;
+        const{value: count}=this.state;
         return count===0?'Zero':count;
     }
 }
