935ac1dc6ea2069604fa09cc2228dd9add4866eb
Razu <jahangir8988@gmail.com>
2019-03-19 16:49:46 +0600

    ok

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import { link } from "fs";
 
-<<<<<<< HEAD
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
   styles = {
     fontSize: 50,
@@ -18,84 +16,40 @@ class Counter extends Component {
   // }
   handleIncrement = product => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
-
+  doHandleIncrement = () => {
+    this.setState({ id: 1 });
+  };
   render() {
+    console.log(this.props);
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 0 })}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
         {/* <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul> */}
+                {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
+            </ul> */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
-=======
-class  Counter extends Component {
-    state = {
-        value: this.props.value
-    };
-    styles = {
-        fontSize:50,
-        fontWeight:"bold"
-    }
-
-    // constructor(){
-    //     super();
-    //     this.handleIncrement=this.handleIncrement.bind(this);
-    // }
-    handleIncrement =(product) =>{
-        console.log(product);
-       this.setState({value:this.state.value+1});
-    }
-    doHandleIncrement = () =>{
-        this.setState({id:1});
-    }
-    render() { 
-        console.log(this.props);
-        return (
-        <div>
-            {this.props.children}
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={ () => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>
-            {/* <ul>
-                {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-            </ul> */}
-        </div>
-        );
-    }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount(){
-        const{value: count}=this.state;
-        return count===0?'Zero':count;
-    }
->>>>>>> 6cbf6fa6b8c1943b122e976eccdb2e651c5d1166
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import Counter from "./counter";
 
@@ -15,36 +14,14 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
+        <h4>Title</h4>
+        <Counter />
       </div>
     );
   }
 }
 
 export default Counters;
-=======
-import React, { Component } from 'react';
-import Counter from './counter';
-
-class Counters extends Component {
-    state = { 
-        counters:[
-            {id:1, value:4},
-            {id:2, value:0},
-            {id:3, value:0},
-            {id:4, value:0},
-        ]
-     }
-    render() { 
-        return ( <div>
-            {this.state.counters.map(counter => 
-            <Counter key={counter.id} value={counter.value} />)}
-            <h4>Title</h4>
-            <Counter/>
-        </div> );
-    }
-}
- 
-export default Counters;
->>>>>>> 6cbf6fa6b8c1943b122e976eccdb2e651c5d1166
+// test push
